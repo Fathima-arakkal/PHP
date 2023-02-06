89fcc511e1c9d50e13296e66e7bbcaedf106da76
Fathima Arakkal <111554827+Fathima-arakkal@users.noreply.github.com>
2023-02-06 15:35:03 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+    <?php  
+    $name=$_POST["name"];//receiving name field value in $name variable  
+    $password=$_POST["password"];//receiving password field value in $password variable  
+      
+    echo "Welcome: $name, your password is: $password";  
+    ?>    
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<?php  
+$name=$_GET["name"];//receiving name field value in $name variable  
+echo "Welcome, $name";  
+?>  
+</body>
+</html>
