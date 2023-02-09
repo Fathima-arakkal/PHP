6b0c514714959a49898a7bfcf1a8a8919b813266
Fathima Arakkal <111554827+Fathima-arakkal@users.noreply.github.com>
2023-02-09 15:19:12 +0530

    Add files via upload
    changed and added date of birth

--- correctvali.php
+++ correctvali.php
@@ -12,6 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
     $phone = test_input($_POST["phone"]);
+    $DOB = test_input($_POST["DOB"]);
     $gender = test_input($_POST["gender"]);
 
 }
@@ -32,6 +33,8 @@ function test_input($data) {
   <br><br>
   PhoneNo: <input type="number" name="phone">
   <br><br>
+  DOB:<input type="date"name="DOB"><br><br>
+  <br><br>
   Gender:
   <input type="radio" name="gender" value="female">Female
   <input type="radio" name="gender" value="male">Male
@@ -48,8 +51,14 @@ echo $email;
 echo "<br>";
 echo $phone;
 echo "<br>";
+echo $DOB;
+echo "<br>";
 echo $gender;
+echo "<br>";
 ?>
-
+<?php
+echo "Date is :" . date("d/m/y") . "<br>";
+echo "Today is : " . date("l");
+?>
 </body>
 </html>
